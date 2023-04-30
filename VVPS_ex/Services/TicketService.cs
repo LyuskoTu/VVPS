05f1f8df0046ad6faf1dd1cdbcbf00b035f6749a
Niki <nl@auxcode.com>
2023-04-30 17:32:16 +0300

    update tests

--- VVPS_ex/Services/TicketService.cs
+++ VVPS_ex/Services/TicketService.cs
@@ -10,20 +10,17 @@ namespace VVPS_ex.Services
 {
     public class TicketService
     {
-        // Define the pricing rules as constants
         private const decimal BasePrice = 100;
         private const decimal DefaultDiscount = 0.95m; // 5% discount
         private const decimal ElderlyDiscount = 0.66m; // 34% discount
         private const decimal ChildDiscount = 0.5m; // 50% discount
         private const decimal FamilyCardDiscount = 0.9m; // 10% discount
 
-        // Define the peak hours as constants
         private static readonly TimeSpan MorningPeakStart = new(7, 30, 0);
         private static readonly TimeSpan MorningPeakEnd = new(9, 30, 0);
         private static readonly TimeSpan AfternoonPeakStart = new(16, 0, 0);
         private static readonly TimeSpan AfternoonPeakEnd = new(19, 30, 0);
 
-        // Define a method to calculate the price of a ticket based on the route and time of travel
         public decimal CalculatePrice(string from, string to, DateTime departureTime, bool isReturnTrip, bool hasElderlyPass, bool hasChild, bool hasFamilyCard)
         {
             if(hasElderlyPass && hasFamilyCard)
@@ -32,14 +29,11 @@ namespace VVPS_ex.Services
             }
             decimal price = BasePrice;
 
-            // Calculate the discount based on the travel time
             if (!IsPeakHour(departureTime))
             {
                 price *= DefaultDiscount;
             }
 
-            // Apply the appropriate discounts based on the passenger types and passes
-            // Можете да притежавате само един вид железопътна карта
             if (hasElderlyPass)
             {
                 price *= ElderlyDiscount;
@@ -53,7 +47,6 @@ namespace VVPS_ex.Services
                 price *= FamilyCardDiscount;
             }
 
-            // Double the price for return trips
             if (isReturnTrip)
             {
                 price *= 2;
@@ -62,8 +55,6 @@ namespace VVPS_ex.Services
             return price;
         }
         
-
-        // Define a helper method to determine if the travel time is during peak hours
         private bool IsPeakHour(DateTime travelTime)
         {
             TimeSpan timeOfDay = travelTime.TimeOfDay;
--- VVPS_ex/Tests/TicketServiceTests.cs
+++ VVPS_ex/Tests/TicketServiceTests.cs
@@ -15,20 +15,22 @@ namespace MVCTest.Tests
         }
 
         [Theory]
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T07:00:00", false, false, false, false, 95)] // Off-peak hour, no discounts
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T07:00:00", true, false, false, false, 190)] // Off-peak hour, no discounts, return trip
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T07:00:00", false, true, false, false, 62.7)] // Off-peak hour, elderly pass
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T07:00:00", false, false, true, true, 47.5)] // Off-peak hour, family card and child
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T07:00:00", false, false, false, true, 85.5)] // Off-peak hour, family card
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T07:00:00", false, false, true, false, 95)] // Off-peak hour, child
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T08:45:00", false, false, false, false, 100)] // Morning peak hour
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T18:30:00", false, false, false, false, 100)] // Afternoon peak hour
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T08:00:00", false, false, false, false, 100)] // peak hour, no discounts
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T08:00:00", true, false, false, false, 200)] // peak hour, no discounts, return trip
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T08:00:00", false, true, false, false, 66)] // peak hour, elderly pass
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T08:00:00", false, false, true, true, 50)] // peak hour, family card and child
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T08:00:00", false, false, false, true, 90)] // peak hour, family card
-        [InlineData("Sofia", "Plovdiv", "2023-05-01T08:00:00", false, false, true, false, 100)] // peak hour, child
+        [InlineData("Sofia", "Plovdiv", "2023-04-25T07:35:00", false, false, false, false, 100)] // Peak hour, no discounts
+        [InlineData("Sofia", "Plovdiv", "2023-05-12T11:30:00", false, false, false, false, 95)] // Off-peak hour, no discounts, first interval
+        [InlineData("Sofia", "Plovdiv", "2023-05-15T23:50:00", false, false, false, false, 95)] // Off-peak hour, no discounts, second interval
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T08:30:00", false, true, false, false, 66)] // Peak hour, elderly discount
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T08:30:00", false, false, true, false, 100)] // Peak hour, has child, no family card
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T08:00:00", false, false, true, true, 50)] // Peak hour, has child, no family card
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T08:00:00", true, true, false, false, 132)] // Peak hour, elderly pass, return trip
+        [InlineData("Sofia", "Plovdiv", "2023-05-05T23:18:00", true, false, false, false, 190)] // Off-peak hour, no discounts, return trip, second interval
+        [InlineData("Sofia", "Plovdiv", "2023-05-05T10:20:00", true, false, false, false, 190)] // Off-peak hour, no discounts, return trip, first interval
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T07:35:00", true, false, false, false, 200)] // Peak hour, no discounts, return trip
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T07:35:00", true, false, true, false, 200)] // Peak hour, no discounts, return trip, has child
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T07:50:00", true, false, true, true, 100)] // Peak hour, return trip, has child, has family card
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T11:35:00", false, false, true, true, 47.5)] // Off-peak hour, has child , has family card
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T10:35:00", false, false, false, true, 85.5)] // Off-peak hour, has family card
+        [InlineData("Sofia", "Plovdiv", "2023-04-06T10:35:00", false, true, false, false, 62.7)] // Off-peak hour, has elderly pass
+
         public void CalculatePrice_ReturnsCorrectPrice(string from, string to, string departureTimeStr, bool isReturnTrip, bool hasElderlyPass, bool hasChild, bool hasFamilyCard, decimal expectedPrice)
         {
             DateTime departureTime = DateTime.Parse(departureTimeStr);
